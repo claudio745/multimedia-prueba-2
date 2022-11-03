1b7d5942cf03366ccde8a7435f02753281b6827b
claudio afaro <74275767+claudio745@users.noreply.github.com>
2022-11-03 12:36:30 -0300

    Create Pagina2.php

---
+++ Pagina2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Inicio</title>
+</head>
+<body>
+
+    <div class="container" style="background-color: gray">
+        <div class="container">
+            <div class="row" style="height: 100px; background-color: blue">
+                <div class="col">Menu de navegacion</div>
+            </div>
+        </div>
+        <div class="container">
+            <h2>Datos personas</h2>
+            <div class="row" style="height: 600px; background-color: red">
+             
+            </div>
+        </div>
+        <div class="container">
+            <div class="row" style="height: 100px; background-color: green">
+                <div class="col">Footer</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php
