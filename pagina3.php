f31b0b94fd53fa04c79543ba36e369ebbcd8b6a7
JMezaV2020 <josemeza@alumnos.upla.cl>
2022-11-03 13:01:10 -0300

    nuevas cosas
    pag nuevas

--- pagina3.php
+++ pagina3.php
@@ -8,13 +8,13 @@
     <!-- Bootstrap CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Página 2</title>
+    <title>Página 3</title>
 </head>
 <body>
     <div class="container" style="background-color: gray">
         <div class="container">
             <div class="row" style="height: 100px; background-color: blue">
-            <div class="col">Menu de navegacion:
+                <div class="col">Menu de navegacion:
                     <a href = "pagina3.php"><button type="button" class="btn btn-link">Inicio</button></a>|
                     <a href = "index.html"><button type="button" class="btn btn-link">Ingresar Persona</button></a>|
                     <a href = "pagina2.php"><button type="button" class="btn btn-link">Lista Persona</button></a>|
@@ -24,7 +24,7 @@
         </div>
         <div class="container">
             <div class="row" style="height: 600px; background-color: red">
-                <div class="col">Tabla</div>
+                <div class="col">Perfil</div>
             </div>
         </div>
         <div class="container">
--- pagina4.php
+++ pagina4.php
@@ -8,13 +8,13 @@
     <!-- Bootstrap CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Página 2</title>
+    <title>Página 4</title>
 </head>
 <body>
     <div class="container" style="background-color: gray">
         <div class="container">
             <div class="row" style="height: 100px; background-color: blue">
-            <div class="col">Menu de navegacion:
+                <div class="col">Menu de navegacion:
                     <a href = "pagina3.php"><button type="button" class="btn btn-link">Inicio</button></a>|
                     <a href = "index.html"><button type="button" class="btn btn-link">Ingresar Persona</button></a>|
                     <a href = "pagina2.php"><button type="button" class="btn btn-link">Lista Persona</button></a>|
@@ -23,8 +23,11 @@
             </div>
         </div>
         <div class="container">
-            <div class="row" style="height: 600px; background-color: red">
-                <div class="col">Tabla</div>
+            <div class="row" style="height: 300px; background-color: red">
+                <div class="col">Estadistica</div>
+            </div>
+            <div class="row" style="height: 300px; background-color: yellow">
+                <div class="col">Grafico</div>
             </div>
         </div>
         <div class="container">
