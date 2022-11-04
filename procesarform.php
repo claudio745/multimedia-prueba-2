b805baad24fdecb8bca19c897dfa5239d7509ad9
claudio afaro <74275767+claudio745@users.noreply.github.com>
2022-11-03 23:38:07 -0300

    Update procesarform.php

--- procesarform.php
+++ procesarform.php
@@ -1,4 +1,23 @@
 <?php
+    require_once('conexion.php');
+    if (empty($_POST['Nombre']) || empty($_POST['Apellido']) || empty($_POST['Rut']) || empty($_POST['fecha']) || empty($_POST['nacionalidad']) || empty($genero = $_POST['genero1']) || empty($_POST['ciudad'])){
+        echo "Por favor llene los campos correspondientes";
+    }else{
+        $nombres = $_POST['Nombres'];
+        $apellidos = $_POST['Apellidos'];
+        $rut = $_POST['Rut'];
+        $fecha_nacim = $_POST['fecha'];
+        $nacionalidad = $_POST['nacionalidad'];
+        $genero = $_POST['genero1'];
+        $ciudad = $_POST['ciudad'];
+        $foto = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+    }
+
+    $query = "INSERT INTO  usuarios (nombre, apellido, imagen) VALUES ('$nombre', '$apellido', '$imagen')";
+    $resultado = $conexion->query($query);
+
+
+    
 
 
 
