09c7a40e3a860a832a209779b233f7d4438731d6
JMezaV2020 <josemeza@alumnos.upla.cl>
2022-11-05 03:12:37 -0300

    Update generarPDF.php

--- generarPDF.php
+++ generarPDF.php
@@ -1,12 +1,32 @@
-<div id="current_date"></p>
-    <script>
-        date = new Date();
-        year = date.getFullYear();
-        month = date.getMonth() + 1;
-        day = date.getDate();
-        hora = date.getHours();
-        minutos = date.getMinutes();
-        segundos = date.getSeconds();
-        document.getElementById("current_date").innerHTML = day + "/" + month + "/" + year + " " + "Hora: " + hora + ":" + minutos + ":" + segundos;
-    </script>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pdf.css">
+
+    <title>PDF</title>
+</head>
+<body>
+    <div class="cuerpo">
+        <div class="arriba"></div>
+        <br></br>
+        <div class="abajo"></div>
+        <div class="hora">
+            <div id="current_date">
+                <script>
+                    date = new Date();
+                    year = date.getFullYear();
+                    month = date.getMonth() + 1;
+                    day = date.getDate();
+                    hora = date.getHours();
+                    minutos = date.getMinutes();
+                    segundos = date.getSeconds();
+                    document.getElementById("current_date").innerHTML = "Fecha: " + day + " / " + month + " / " + year + " " + "Hora: " + hora + ":" + minutos + ":" + segundos;
+                </script>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
