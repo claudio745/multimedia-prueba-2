d978ad55f122b56400228cbe52e1f5c5a9ec7280
claudio afaro <74275767+claudio745@users.noreply.github.com>
2022-11-04 22:38:55 -0300

    Update procesarform.php

--- procesarform.php
+++ procesarform.php
@@ -1,6 +1,6 @@
 <?php
     require_once('conexion.php');
-    if (empty($_POST['Nombre']) || empty($_POST['Apellido']) || empty($_POST['Rut']) || empty($_POST['fecha']) || empty($_POST['nacionalidad']) || empty($genero = $_POST['genero1']) || empty($_POST['ciudad'])){
+    if (empty($_POST['Nombres']) || empty($_POST['Apellidos']) || empty($_POST['Rut']) || empty($_POST['ciudad'])){
         echo "Por favor llene los campos correspondientes";
     }else{
         $nombres = $_POST['Nombres'];
@@ -11,17 +11,15 @@
         $genero = $_POST['genero1'];
         $ciudad = $_POST['ciudad'];
         $foto = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
-    }
-
-    $query = "INSERT INTO  usuarios (nombre, apellido, imagen) VALUES ('$nombre', '$apellido', '$imagen')";
-    $resultado = $conexion->query($query);
 
 
+        $query = "INSERT INTO  datos (nombres, apellidos, rut, fecha_nacim, nacionalidad, genero, ciudad_residencia , foto) VALUES ('$nombres', '$apellidos', '$rut', '$fecha_nacim', '$nacionalidad', '$genero', '$ciudad' , '$foto')";
+        $resultado = $conexion->query($query);
     
-
-
-
-
-
-
-
+        if($resultado){
+            echo "Se han insertado los datos correctamente";
+        }else{
+            echo "No se han guardado los datos";
+        }
+    
+    }
