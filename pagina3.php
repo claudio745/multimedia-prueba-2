0628541534b81a5f6e39fb3469cdac0f6e8e1939
JMezaV2020 <josemeza@alumnos.upla.cl>
2022-11-04 22:39:17 -0300

    Update pagina3.php

--- pagina3.php
+++ pagina3.php
@@ -15,7 +15,7 @@
     <div class="container" style="background-color: gray">
         <div class="row caja"></div>
         <div class="container">
-            <div class="row" style="height: 100px; background-color: blue">
+            <div class="row" style="height: 100px; background-color: #d9afa2">
                 <div class="col d-flex justify-content-center">Menu de navegacion:
                     <a href = "pagina3.php"><button type="button" class="btn btn-link">Inicio</button></a>|
                     <a href = "index.php"><button type="button" class="btn btn-link">Ingresar Persona</button></a>|
@@ -26,13 +26,13 @@
         </div>
         <div class="row caja2"></div>
         <div class="container">
-            <div class="row" style="height: 600px; background-color: red">
+            <div class="row" style="height: 600px; background-color: #E3DAC9">
                 <div class="col">Perfil</div>
             </div>
         </div>
         <div class="row caja2"></div>
         <div class="container">
-            <div class="row" style="height: 100px; background-color: green">
+            <div class="row" style="height: 100px; background-color: #B0E0DA">
                 <div class="col">Tecnología Multimedia CIF 6459-1, Fecha de entrega: 06/11/2022, Nombre integrantes: Jose Meza, Claudio Alfaro</div>
             </div>
         </div>
