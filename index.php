cc120357ddc7920c4800d9af7104773089d16213
JMezaV2020 <josemeza@alumnos.upla.cl>
2022-11-04 23:16:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
                 </div>
             </div>
         </div>
-        <div class="row caja2"></div>
+        <div class="row caja"></div>
         <div class="container">
             <div class="row" style="height: 600px; background-color: #E3DAC9">
                 <div class="col-8"> <h3>Formulario</h3>
@@ -64,7 +64,7 @@
                 <div class="col-4">LINK</div>
             </div>
         </div>
-        <div class="row caja2"></div>
+        <div class="row caja"></div>
         <div class="container">
             <div class="row" style="height: 100px; background-color: #B0E0DA">
                 <div class="col">Tecnología Multimedia CIF 6459-1, Fecha de entrega: 06/11/2022, Nombre integrantes: Jose Meza, Claudio Alfaro</div>
