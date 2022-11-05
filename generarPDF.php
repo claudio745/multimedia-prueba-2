5dc1a786494aa01f108ca535a5326f6824f6b899
JMezaV2020 <josemeza@alumnos.upla.cl>
2022-11-05 14:47:30 -0300

    Update generarPDF.php

--- generarPDF.php
+++ generarPDF.php
@@ -1,30 +1,10 @@
-<?php 
+<?php
+require ('fpdf/fpdf.php');
 
-function crearPDF($nombre)
-{
-	ob_start();
-	include('pdf.php');
-	$content = ob_get_clean();
-
-
-	try {
-
-		require_once('html2pdf-5.2.5');
-		$html2pdf = new HTML2PDF('P', 'LETTER', 'es', true, 'UTF-8');
-		$html2pdf->pdf->SetDisplayMode('fullpage');
-
-		$html2pdf->pdf->SetTitle("PDF");
-
-		$html2pdf->writeHTML($content);
-		ob_end_clean();
-
-		$html2pdf->Output($nombre.'.pdf', 'D');
-		
-	} catch (HTML2PDF_exception $e) {
-		echo $e;
-		exit;
-	}
+class PDF extends FPDF{
+    include ('pdf.php');
 }
-
-crearPDF("pdf");
- ?>
+$pdf = new PDF();
+$pdf->SetFont('Times', '', 12);
+$pdf->OutPut('Reporte.PDF', 'I');
+?>
